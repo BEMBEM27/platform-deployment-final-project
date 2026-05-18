4acca827ad29f308c1254f81ab60f9a5a3301afe
BEMBEM27 <mariabromano97@gmail.com>
2026-05-18 21:30:41 +0800

    fix: include nginx config and entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
-set -e
-cd /var/www/html
 
-if [ "$APP_ENV" = "prod" ]; then
-    php bin/console cache:clear --env=prod --no-warmup
-    php bin/console cache:warmup --env=prod --no-debug
-fi
+# Run database migrations
+php bin/console doctrine:migrations:migrate --no-interaction
 
-# Patuyukon ang database migration
-php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
-
-# Sugdan ang PHP-FPM sa background ug i-foreground si Nginx
+# Start PHP-FPM in background
 php-fpm -D
+
+# I-start ang Nginx sa foreground aron magpabilin buhi ang container
 nginx -g "daemon off;"
